d1eeebcc017289f1ffc0a11d39a38f92e915aaf3
sgagliasso <simonegagliasso94@gmail.com>
2025-05-27 17:04:09 +0200

    feat: completato l' esercizio

---
+++ java-festa-ferragnez/src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,40 @@
+package com.ferragnez.party;
+// Consegna:
+// creare e inizializzare l’array contenente i nomi degli invitati
+// chiedere all’utente come si chiama
+// verificare che il nome sia presente nella lista
+// lasciarlo entrare o rispedirlo cortesemente da dove è venuto
+import java.util.Scanner;
+public class CheckGuest {
+    public static void main(String[] args) throws Exception {
+        // creo array di stringhe contenente gli invitati
+        String[] listaInvitati = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+
+        // chiedere all' invitato il nome e il cognome e salvarlo in una variabile
+        Scanner input = new Scanner(System.in);
+        System.out.println("Qual è il suo nome e cognome?");
+        String nomeInvitato = input.nextLine();
+
+        //verificare che la variabile stringa sia contenuta nell' array
+        Boolean eInvitato = false;
+        int i = 0;
+        for(i = 0 ; i < listaInvitati.length ; i++){
+            //se presente stampare sul terminale "Puoi entrare!" viceversa "Non può entrare!""
+            if(nomeInvitato.equals(listaInvitati[i])){
+                eInvitato = true;
+                break;
+            }
+        }
+
+        if(eInvitato == true){
+            System.out.println("Puoi entrare!");
+        }else{
+            System.out.println("Non puoi entrare!");
+        }
+        
+    
+       
+       
+        input.close();
+    }
+}
